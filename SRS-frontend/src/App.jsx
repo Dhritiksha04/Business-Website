716973a37e3ae22623b8f7d2d203e48079a715a6
Dhritiksha04 <144090937+Dhritiksha04@users.noreply.github.com>
2025-05-27 16:39:55 +0530

    Update App.jsx

--- SRS-frontend/src/App.jsx
+++ SRS-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import { CssBaseline } from '@mui/material';
 import About from './pages/About';
 import Home from './pages/Home';
 import Service from './pages/Service';
